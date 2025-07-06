86b7b19177f4bc01608c48479db6f5149f7a81f8
IamAshish2 <karkiaashish9899@gmail.com>
2025-07-06 21:11:46 +0545

    update to use AspNetCore namespace

--- JobApplicationTracker.Api/GlobalExceptionHandler/GlobalExceptionHandler.cs
+++ JobApplicationTracker.Api/GlobalExceptionHandler/GlobalExceptionHandler.cs
@@ -1,5 +1,5 @@
 using System.Net;
-using JobApplicationTracker.Service.Exceptions;
+using JobApplicationTracker.Service.Exceptions; 
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 
@@ -8,18 +8,19 @@ namespace JobApplicationTracker.Api.GlobalExceptionHandler;
 public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger, IHostEnvironment hostEnvironment)
     : IExceptionFilter
 {
-
     public void OnException(ExceptionContext context)
     {
         // Log the exception
         logger.LogError(context.Exception, "An unhandled exception occurred: {Message}", context.Exception.Message);
-        
-        // Prevent further exception handling by other filters
+
+        // Prevent further exception handling by other filters/middleware 
         context.ExceptionHandled = true;
-        
+
         HttpStatusCode statusCode = HttpStatusCode.InternalServerError;
         string message = "An unexpected error occurred.";
-        string details = hostEnvironment.IsDevelopment() ? context.Exception.Message : "";
+        
+        // provide full exception details in development environement
+        string? details = hostEnvironment.IsDevelopment() ? context.Exception.ToString() : null; 
         var errors = new List<string>();
 
         switch (context.Exception)
@@ -27,34 +28,47 @@ public class GlobalExceptionHandler(ILogger<GlobalExceptionHandler> logger, IHos
             case NotFoundException notFoundException:
                 statusCode = HttpStatusCode.NotFound;
                 message = notFoundException.Message;
+                details = hostEnvironment.IsDevelopment() ? notFoundException.ToString() : notFoundException.Message;
                 break;
 
             case ValidationException validationException:
                 statusCode = HttpStatusCode.BadRequest;
                 message = validationException.Message;
                 errors.AddRange(validationException.Errors);
+                details = hostEnvironment.IsDevelopment() ? validationException.ToString() : validationException.Message;
                 break;
 
             case ArgumentException argEx:
                 statusCode = HttpStatusCode.BadRequest;
                 message = argEx.Message;
+                details = hostEnvironment.IsDevelopment() ? argEx.ToString() : argEx.Message;
                 break;
 
             case UnauthorizedAccessException _:
                 statusCode = HttpStatusCode.Unauthorized;
                 message = "Access denied. You are not authorized to perform this action.";
+                details = hostEnvironment.IsDevelopment() ? context.Exception.ToString() : message;
                 break;
         }
 
-        // using ProblemDetails for standard error response
+        // Using ProblemDetails for standard error response 
         var response = new ProblemDetails()
         {
             Status = (int)statusCode,
             Title = message,
-            Detail = details,
-            Instance = context.HttpContext.Request.Path
+            Detail = details, 
+            Instance = context.HttpContext.Request.Path 
         };
+        
+        
+        if (errors.Any())
+        {
+            response.Extensions["errors"] = errors;
+        }
 
-        context.Result = new JsonResult(response);
+        context.Result = new JsonResult(response)
+        {
+            StatusCode = (int)statusCode 
+        };
     }
 }
